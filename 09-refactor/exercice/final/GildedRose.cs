4124533cdeaac51530d8d6560d9210d0a1b0ce7a
Mickael Metesreau <mickael.metesreau@gmail.com>
2021-06-08 18:28:21 +0200

    refacto 1 final

---
+++ 09-refactor/exercice/final/GildedRose.cs
@@ -0,0 +1,87 @@
+﻿using System.Collections.Generic;
+using static csharpcore.ItemType;
+
+namespace csharpcore
+{
+    static class QualityItemUpdateStrategyFactory
+    {
+        public static IQualityItemUpdateStrategy Create(string name)
+        {
+            return name switch
+            {
+                AgedBrie => new AgedBrieQualityUpdate(),
+                BackstagePasses => new BackstagePasseQualityUpdate(),
+                _ => new RegularItemQualityUpdate()
+            };
+        }
+    }
+
+    public interface IQualityItemUpdateStrategy
+    {
+        void Update(Item item);
+    }
+
+    class AgedBrieQualityUpdate : IQualityItemUpdateStrategy
+    {
+        public void Update(Item item)
+        {
+            item.IncreaseQuality();
+
+            if (item.SellIn < 0)
+                item.IncreaseQuality();
+        }
+    }
+
+    class BackstagePasseQualityUpdate : IQualityItemUpdateStrategy
+    {
+        public void Update(Item item)
+        {
+            item.IncreaseQuality();
+
+            if (item.SellIn < 10)
+                item.IncreaseQuality();
+
+            if (item.SellIn < 5)
+                item.IncreaseQuality();
+
+            if (item.SellIn < 0)
+                item.Quality = 0;
+        }
+    }
+
+    class RegularItemQualityUpdate : IQualityItemUpdateStrategy
+    {
+        public void Update(Item item)
+        {
+            item.DecreaseQuality();
+
+            if (item.SellIn < 0)
+                item.DecreaseQuality();
+        }
+    }
+
+    public class GildedRose
+    {
+        readonly IList<Item> Items;
+
+        public GildedRose(IList<Item> items)
+        {
+            Items = items;
+        }
+
+        public void UpdateQuality()
+        {
+            foreach (var item in Items)
+                UpdateQualityItem(item);
+        }
+
+        private static void UpdateQualityItem(Item item)
+        {
+            item.DecreaseSellIn();
+
+            var qualityItemUpdateStrategy = QualityItemUpdateStrategyFactory.Create(item.Name);
+
+            item.UpdateQuality(qualityItemUpdateStrategy);
+        }
+    }
+}
---
+++ 09-refactor/exercice/final/Item.cs
@@ -0,0 +1,47 @@
+﻿using static csharpcore.ItemType;
+
+namespace csharpcore
+{
+    static class ItemType
+    {
+        public const string AgedBrie = "Aged Brie";
+        public const string BackstagePasses = "Backstage passes to a TAFKAL80ETC concert";
+        public const string Sulfuras = "Sulfuras, Hand of Ragnaros";
+    }
+
+    public class Item
+    {
+        private const int maxQualityItem = 50;
+        private const int minQualityItem = 0;
+        
+        public string Name { get; set; }
+        public int SellIn { get; set; }
+        public int Quality { get; set; }
+
+        public void IncreaseQuality()
+        {
+            if (Quality < maxQualityItem)
+                Quality += 1;
+        }
+
+        public void DecreaseQuality()
+        {
+            if (Name == Sulfuras) return;
+
+            if (Quality > minQualityItem)
+                Quality -= 1;
+        }
+
+        public void DecreaseSellIn()
+        {
+            if (Name == Sulfuras) return;
+
+            SellIn -= 1;
+        }
+
+        public void UpdateQuality(IQualityItemUpdateStrategy qualityItemUpdateStrategy)
+        {
+            qualityItemUpdateStrategy.Update(this);
+        }
+    }
+}
