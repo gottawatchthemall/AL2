1444e37219e069bfd2b902005fb6a3d9d88d395a
Mickael Metesreau <mickael.metesreau@gmail.com>
2021-06-08 18:40:08 +0200

    init refacto2

---
+++ 09-refactor/02-exercice/init/Game.cs
@@ -0,0 +1,211 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace csharpcore
+{
+    public class Game
+    {
+        public static List<string> lines = new List<string>();
+        List<string> players = new List<string>();
+
+        int[] places = new int[6];
+        int[] purses = new int[6];
+
+        bool[] inPenaltyBox = new bool[6];
+
+        LinkedList<string> popQuestions = new LinkedList<string>();
+        LinkedList<string> scienceQuestions = new LinkedList<string>();
+        LinkedList<string> sportsQuestions = new LinkedList<string>();
+        LinkedList<string> rockQuestions = new LinkedList<string>();
+
+        int currentPlayer = 0;
+        bool isGettingOutOfPenaltyBox;
+
+        public Game()
+        {
+            for (int i = 0; i < 50; i++)
+            {
+                popQuestions.AddLast("Pop Question " + i);
+                scienceQuestions.AddLast(("Science Question " + i));
+                sportsQuestions.AddLast(("Sports Question " + i));
+                rockQuestions.AddLast(createRockQuestion(i));
+            }
+        }
+
+        public String createRockQuestion(int index)
+        {
+            return "Rock Question " + index;
+        }
+
+        public bool isPlayable()
+        {
+            return (howManyPlayers() >= 2);
+        }
+
+        public bool add(String playerName)
+        {
+            players.Add(playerName);
+            places[howManyPlayers()] = 0;
+            purses[howManyPlayers()] = 0;
+            inPenaltyBox[howManyPlayers()] = false;
+
+            Console.WriteLine(playerName + " was added");
+            Console.WriteLine("They are player number " + players.Count);
+            return true;
+        }
+
+        public int howManyPlayers()
+        {
+            return players.Count;
+        }
+
+        public void roll(int roll)
+        {
+            WriteLine(players[currentPlayer] + " is the current player");
+            WriteLine("They have rolled a " + roll);
+
+            if (inPenaltyBox[currentPlayer])
+            {
+                if (roll % 2 != 0)
+                {
+                    isGettingOutOfPenaltyBox = true;
+
+                    WriteLine(players[currentPlayer] + " is getting out of the penalty box");
+                    places[currentPlayer] = places[currentPlayer] + roll;
+                    if (places[currentPlayer] > 11) places[currentPlayer] = places[currentPlayer] - 12;
+
+                    WriteLine(players[currentPlayer]
+                                      + "'s new location is "
+                                      + places[currentPlayer]);
+                    WriteLine("The category is " + currentCategory());
+                    askQuestion();
+                }
+                else
+                {
+                    WriteLine(players[currentPlayer] + " is not getting out of the penalty box");
+                    isGettingOutOfPenaltyBox = false;
+                }
+            }
+            else
+            {
+                places[currentPlayer] = places[currentPlayer] + roll;
+                if (places[currentPlayer] > 11) places[currentPlayer] = places[currentPlayer] - 12;
+
+                WriteLine(players[currentPlayer]
+                                  + "'s new location is "
+                                  + places[currentPlayer]);
+                WriteLine("The category is " + currentCategory());
+                askQuestion();
+            }
+        }
+
+        private void askQuestion()
+        {
+            if (currentCategory() == "Pop")
+            {
+                WriteLine(popQuestions.First());
+                popQuestions.RemoveFirst();
+            }
+
+            if (currentCategory() == "Science")
+            {
+                WriteLine(scienceQuestions.First());
+                scienceQuestions.RemoveFirst();
+            }
+
+            if (currentCategory() == "Sports")
+            {
+                WriteLine(sportsQuestions.First());
+                sportsQuestions.RemoveFirst();
+            }
+
+            if (currentCategory() == "Rock")
+            {
+                WriteLine(rockQuestions.First());
+                rockQuestions.RemoveFirst();
+            }
+        }
+
+
+        private String currentCategory()
+        {
+            if (places[currentPlayer] == 0) return "Pop";
+            if (places[currentPlayer] == 4) return "Pop";
+            if (places[currentPlayer] == 8) return "Pop";
+            if (places[currentPlayer] == 1) return "Science";
+            if (places[currentPlayer] == 5) return "Science";
+            if (places[currentPlayer] == 9) return "Science";
+            if (places[currentPlayer] == 2) return "Sports";
+            if (places[currentPlayer] == 6) return "Sports";
+            if (places[currentPlayer] == 10) return "Sports";
+            return "Rock";
+        }
+
+        public bool wasCorrectlyAnswered()
+        {
+            if (inPenaltyBox[currentPlayer])
+            {
+                if (isGettingOutOfPenaltyBox)
+                {
+                    WriteLine("Answer was correct!!!!");
+                    purses[currentPlayer]++;
+                    WriteLine(players[currentPlayer]
+                                      + " now has "
+                                      + purses[currentPlayer]
+                                      + " Gold Coins.");
+
+                    bool winner = didPlayerWin();
+                    currentPlayer++;
+                    if (currentPlayer == players.Count) currentPlayer = 0;
+
+                    return winner;
+                }
+                else
+                {
+                    currentPlayer++;
+                    if (currentPlayer == players.Count) currentPlayer = 0;
+                    return true;
+                }
+            }
+            else
+            {
+                WriteLine("Answer was corrent!!!!");
+                purses[currentPlayer]++;
+                WriteLine(players[currentPlayer]
+                                  + " now has "
+                                  + purses[currentPlayer]
+                                  + " Gold Coins.");
+
+                bool winner = didPlayerWin();
+                currentPlayer++;
+                if (currentPlayer == players.Count) currentPlayer = 0;
+
+                return winner;
+            }
+        }
+
+        public bool wrongAnswer()
+        {
+            WriteLine("Question was incorrectly answered");
+            WriteLine(players[currentPlayer] + " was sent to the penalty box");
+            inPenaltyBox[currentPlayer] = true;
+
+            currentPlayer++;
+            if (currentPlayer == players.Count) currentPlayer = 0;
+            return true;
+        }
+
+        private void WriteLine(string questionWasIncorrectlyAnswered)
+        {
+            lines.Add(questionWasIncorrectlyAnswered);
+            Console.WriteLine(questionWasIncorrectlyAnswered);
+        }
+
+
+        private bool didPlayerWin()
+        {
+            return !(purses[currentPlayer] == 6);
+        }
+    }
+}
---
+++ 09-refactor/02-exercice/init/Program.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using Xunit;
+
+namespace csharpcore
+{
+    public class Program
+    {
+        private static bool notAWinner;
+
+        public static void Run(string[] args)
+        {
+            Game aGame = new Game();
+
+            aGame.add("Chet");
+            aGame.add("Pat");
+            aGame.add("Sue");
+
+            Random rand = new Random();
+
+            do
+            {
+                var randomValue = rand.Next(5);
+
+                aGame.roll(randomValue + 1);
+
+                if (rand.Next(9) == 7)
+                {
+                    notAWinner = aGame.wrongAnswer();
+                }
+                else
+                {
+                    notAWinner = aGame.wasCorrectlyAnswered();
+                }
+            } while (notAWinner);
+        }
+    }    
+}
+
