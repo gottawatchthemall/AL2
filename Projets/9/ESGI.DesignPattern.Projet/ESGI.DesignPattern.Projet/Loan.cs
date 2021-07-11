654e19c2a9a628a3c08b76934d904b86df2f243a
MasatakaIshii <masaradio78@gmail.com>
2021-07-11 16:55:19 +0200

    WIP cleaning

--- Projets/9/ESGI.DesignPattern.Projet/ESGI.DesignPattern.Projet/Loan.cs
+++ Projets/9/ESGI.DesignPattern.Projet/ESGI.DesignPattern.Projet/Loan.cs
@@ -7,48 +7,39 @@ namespace ESGI.DesignPattern.Projet
     {
         double _commitment;
         private DateTime? _expiry;
-        private DateTime? _maturity;
         IList<Payment> _payments = new List<Payment>();
         private DateTime? _today;
         private DateTime _start;
         private double _riskRating;
         private double _unusedPercentage;
-        private CapitalStrategy _capitalStrategy;
 
         public Loan(double commitment,
                     DateTime start,
                     DateTime? expiry,
-                    DateTime? maturity,
-                    int riskRating,
-                    CapitalStrategy capitalStrategy)
+                    int riskRating)
         {
             this._expiry = expiry;
             this._commitment = commitment;
             this._today = null;
             this._start = start;
-            this._maturity = maturity;
             this._riskRating = riskRating;
             this._unusedPercentage = 1.0;
-            this._capitalStrategy = capitalStrategy;
         }
 
         public static Loan NewTermLoan(double commitment, DateTime start, DateTime maturity, int riskRating)
         {
-            return new Loan(commitment,  start, null,
-                            maturity, riskRating, new CapitalStrategyTermLoan());
+            return new Loan(commitment,  start, null,riskRating);
         }
 
         public static Loan NewRevolver(double commitment, DateTime start, DateTime expiry, int riskRating)
         {
-            return new Loan(commitment,  start, expiry,
-                            null, riskRating, new CapitalStrategyRevolver());
+            return new Loan(commitment,  start, expiry, riskRating);
         }
 
         public static Loan NewAdvisedLine(double commitment, DateTime start, DateTime expiry, int riskRating)
         {
             if (riskRating > 3) return null;
-            Loan advisedLine = new Loan(commitment, start, expiry,
-                                        null, riskRating, new CapitalStrategyAdvisedLine());
+            Loan advisedLine = new Loan(commitment, start, expiry,riskRating);
             advisedLine.SetUnusedPercentage(0.1);
             return advisedLine;
         }
@@ -63,11 +54,6 @@ namespace ESGI.DesignPattern.Projet
             return _commitment;
         }
 
-        public DateTime? GetMaturity()
-        {
-            return _maturity;
-        }
-
         public double GetRiskRating()
         {
             return _riskRating;
@@ -76,11 +62,6 @@ namespace ESGI.DesignPattern.Projet
         public void Payment(double amount, DateTime paymentDate)
         {
             _payments.Add(new Payment(amount, paymentDate));
-        }
-
-        public double Capital()
-        {
-            return _capitalStrategy.Capital(this);
         }
 
         public DateTime? GetToday()
